3ce7da961c70c81ff54003d6da38ce49408648cc
hasan <mdhasanhk.98@gmail.com>
2023-03-15 19:36:23 +0600

    Coding Challenge #1

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,14 @@
 'use strict';
-const person = function (firstName, birthYear) {
+/*
+///////////////////////////////////////
+// Constructor Functions and the new Operator
+
+const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
 
-const jonas = new person('Jonas', 1995);
+const jonas = new Person('Jonas', 1995);
 console.log(jonas);
 
 //New {}  is created
@@ -26,3 +30,73 @@ person.prototype.calcAge = function () {
 
 jonas.calcAge();
 manila.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === person.prototype);
+
+console.log(person.prototype.isPrototypeOf(jonas));
+console.log(person.prototype.isPrototypeOf(manila));
+console.log(person.prototype.isPrototypeOf(person));
+///////////////////////////////////////
+*/
+
+/*
+///////////////////////////////////////
+// Coding Challenge #1
+
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+///////////////////////////////////////
+*/
+
+///////////////////////////////////////
+//class expression
+// const PersonCl = class {};
+
+//class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  colcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+console.log(PersonCl.prototype);
+const jessica = new PersonCl('jessica', 1995);
+console.log(jessica);
+
+jessica.colcAge();
