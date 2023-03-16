54dd9caf1428747d77b0bd0ff6592b693b6f59f4
hasan <mdhasanhk.98@gmail.com>
2023-03-16 11:16:47 +0600

    Setters and Getters

--- starter/script.js
+++ starter/script.js
@@ -118,3 +118,25 @@ jessica.greet();
 // 3. classes are executed in strict mode
 ///////////////////////////////////////
 */
+
+/*
+///////////////////////////////////////
+const account = {
+  owner: 'Jonas',
+  movements: [200, 300, 450, 500, 600],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+///////////////////////////////////////
+*/
