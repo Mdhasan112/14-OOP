fb35cec760d5951376e197380b7badfb459c385d
hasan <mdhasanhk.98@gmail.com>
2023-03-19 20:42:42 +0600

    Inheritance Between Classes: Object.create

--- starter/script.js
+++ starter/script.js
@@ -143,7 +143,7 @@ console.log(account.movements);
 
 /*
 ///////////////////////////////////////
-const personProto = {
+const PersonProto = {
   calcAge() {
     console.log(2025 - this.birthYear);
   },
@@ -153,14 +153,14 @@ const personProto = {
   },
 };
 
-const steven = Object.create(personProto);
+const steven = Object.create(PersonProto);
 console.log(steven);
 
 steven.name = 'Steven';
 steven.birthYear = 2000;
 steven.calcAge();
 
-const sarah = Object.create(personProto);
+const sarah = Object.create(PersonProto);
 
 sarah.init('Steven', 1995);
 sarah.calcAge();
@@ -350,5 +350,40 @@ const martha = new studentC1('Martha', 2010, 'Computer Science');
 console.log(martha);
 martha.introduce();
 martha.calcAge();
+///////////////////////////////////////
+*/
+
+/*
+///////////////////////////////////////
+//Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2025 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+
+jay.introduce();
+jay.calcAge();
 ///////////////////////////////////////
 */
